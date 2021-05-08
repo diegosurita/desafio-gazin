3b3db99bdc9582e173871ef625f95118ed3f0404
Diego Surita <suritadev@gmail.com>
2021-05-08 12:42:16 -0300

    refactor: corrige configurações das colunas na tabela "developers"
    Issue: #4

--- etc/setup-db.sql
+++ etc/setup-db.sql
@@ -2,10 +2,10 @@
 USE gazin;
 
 CREATE TABLE IF NOT EXISTS developers (
-    developer_id INT NOT NULL PRIMARY KEY,
+    developer_id INT NOT NULL PRIMARY KEY auto_increment,
     name VARCHAR(200) NOT NULL,
     sex CHAR(1),
     age INT(3),
     hobby VARCHAR(255),
-    birth_date DATETIME
+    birthdate DATE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
