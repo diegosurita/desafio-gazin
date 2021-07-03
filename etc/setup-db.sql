35c5a8732f1ed185a1306d6af0d555abe24c39c7
Diego Surita <suritadev@gmail.com>
2021-07-03 08:08:34 -0300

    refactor: Ajusta o setup inicial do banco de dados e configura a inicialização do mesmo no docker compose
    Issue: #2

--- etc/setup-db.sql
+++ etc/setup-db.sql
@@ -1,6 +1,7 @@
 -- Script cria configura tabelas do banco de dados
 USE gazin;
 
+# developers
 CREATE TABLE IF NOT EXISTS developers (
     developer_id INT NOT NULL PRIMARY KEY auto_increment,
     name VARCHAR(200) NOT NULL,
@@ -8,7 +9,7 @@ CREATE TABLE IF NOT EXISTS developers (
     age INT(3),
     hobby VARCHAR(255),
     birthdate DATE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO developers (
     name, sex, age, hobby, birthdate
